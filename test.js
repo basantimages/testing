d41eee9c09c3ff64c8a4c4078c8529fae766d6ca
Basantraj Shakti <basantrajshakti@gmail.com>
2022-04-15 17:31:53 +0530

    01

---
+++ test.js
@@ -0,0 +1,10 @@
+async function test() {
+  const res = await fetch('https://instagram.com/hello/?__a=1&__d=dis');
+
+  if (res.ok) {
+    const rs = await res.json();
+    console.log(rs);
+  } else {
+    console.log('not ok');
+  }
+}
